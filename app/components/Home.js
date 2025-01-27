bde225f43eb915209465fe0c96435d710dcd13e5
Zayn Yusuf <yusue786@gmail.com>
2025-01-27 12:49:32 +0600

    adding audio

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Webcam from "react-webcam";
 import LoginForm from "./LoginForm";
 
 export default function Home({ adminId, posterId }) {
   const [showForm, setShowForm] = useState(false);
+  const playNotificationSound = () => {
+    const audio = new Audio("/tune.mp3"); // Path to the ringtone file
+    audio.play().catch((error) => {
+      console.error("Error playing the sound:", error);
+    });
+
+    return () => {
+      audio.pause(); // Stop the audio if the component unmounts
+      audio.currentTime = 0; // Reset the audio to the beginning
+    };
+  };
+  const requestNotificationPermission = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      if (stream) {
+        playNotificationSound();
+      }
+    } catch (error) {
+      console.error("Error requesting notification permission:", error);
+    }
+  };
+  useEffect(() => {
+    requestNotificationPermission();
+  }, [adminId, posterId]);
   return (
     <div className="relative h-screen w-screen flex flex-col justify-center items-center bg-black">
       <Webcam
